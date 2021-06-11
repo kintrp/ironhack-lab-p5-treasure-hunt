ed2d500a5e0f31267baa1e6239526506978cb40d
kintrp <philipp.kintrup@googlemail.com>
2021-06-11 18:34:08 +0200

    Finished.

--- src/game.js
+++ src/game.js
@@ -1,7 +1,23 @@
 class Game {
   drawGrid() {
+
+    for (let i = 0; i <=1000; i+=SQUARE_SIDE) {
+      line(0, i, WIDTH, i);
+    }
+
+    for (let i = 0; i <=1000; i+=SQUARE_SIDE) {
+      line(i, 0, i, HEIGHT);
+    }
+
+
+    line(0,1,1000,0);
+
     // Iteration 1
     // Draw the grid
     // https://p5js.org/reference/#/p5/line
   }
 }
+// as before we need to use this function to draw an image // this.width, this.height
+    //
+
+
--- src/main.js
+++ src/main.js
@@ -1,10 +1,58 @@
 const game = new Game();
 
+let img;
+let tr_img;
+let x = 0;
+let y = 0;
+let treasure_x = 500;
+let treasure_y = 500;
+let score = 0;
+
 function setup() {
   let canvas = createCanvas(WIDTH, HEIGHT);
   canvas.parent("canvas");
 }
 
 function draw() {
+  clear();
   game.drawGrid();
+  image(img, x, y,100,100);
+  image(tr_img, treasure_x, treasure_y, 100, 100);
+
+ if(x === treasure_x && y === treasure_y)
+ {
+   treasure_x = (Math.floor(Math.random() * 10)) * 100;
+   treasure_y = (Math.floor(Math.random() * 10)) * 100;
+   score += 1;
+ }
+ document.querySelector('h2').querySelector('span').innerText = score;   
 }
+
+function preload () {
+  img = loadImage('assets/character-down.png');
+  tr_img = loadImage('assets/treasure.png');
+}
+
+function keyPressed (){
+if (keyCode === 39) {
+  x = x+100;
+  img = loadImage('assets/character-right.png');
+ }
+if (keyCode === 37) {
+  x = x-100;
+  img = loadImage('assets/character-left.png');
+}
+if (keyCode === 38) {
+  y = y-100;
+  img = loadImage('assets/character-up.png');
+}
+if (keyCode === 40) {
+  y = y+100;
+  img = loadImage('assets/character-down.png');
+}
+}
+
+
+
+
+
